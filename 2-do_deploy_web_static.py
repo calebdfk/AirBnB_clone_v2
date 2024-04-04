71cba84b877823d8c3034c692b68c1a227bc09dc
Yator1 <titusyatz17@gmail.com>
2024-04-04 10:18:45 -0700

    Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Fabric script  that distributes an archive to your web servers
+"""
+
+from fabric.api import put, run, env
+from os.path import exists
+env.hosts = ['54.157.184.104', '54.157.186.151']
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to web servers.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("Deployment succesful")
+        return True
+
+    return False
