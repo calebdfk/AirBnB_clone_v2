5072cb48d1341d2d0e031a985398423dd49b4dd4
calebdfk <dkweteza@outlook.com>
2024-04-24 15:33:17 -0700

    update models

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,6 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
 
-
 class Amenity(BaseModel, Base):
     """Represents an Amenity for a MySQL database.
 
@@ -17,5 +16,4 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    place_amenities = relationship("Place", secondary="place_amenity", viewonly=False, extend_existing=True)
--- models/place.py
+++ models/place.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 """Defines the Place class."""
-from os import getenv
 
+from os import getenv
 from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
-
 import models
 from models.base_model import Base, BaseModel
 from models.amenity import Amenity
 from models.review import Review
-from models.place import Place
 
-association_table = Table("place_amenity", Base.metadata,
-                          Column("place_id", String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True, nullable=False),
-                          Column("amenity_id", String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True, nullable=False))
-
+association_table = Table(
+    "place_amenity", Base.metadata,
+    Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
+    Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False),
+    extend_existing=True  # Add this line
+)
 
 class Place(BaseModel, Base):
     """Represents a Place for a MySQL database.
-
+    
     Inherits from SQLAlchemy Base and links to the MySQL table places.
-
+    
     Attributes:
         __tablename__ (str): The name of the MySQL table to store places.
         city_id (sqlalchemy String): The place's city id.
@@ -53,8 +49,7 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     reviews = relationship("Review", backref="place", cascade="delete")
-    amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False)
+    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
@@ -80,3 +75,4 @@ class Place(BaseModel, Base):
         def amenities(self, value):
             if isinstance(value, Amenity):
                 self.amenity_ids.append(value.id)
+
